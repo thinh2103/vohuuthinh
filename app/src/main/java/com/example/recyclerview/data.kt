66b1bf3d16c144d5b7af997d04f2d67654fb33a8
thinh2103 <vhthinh.20it8@vku.udn.vn>
2022-03-15 13:45:14 +0700

    first commit

---
+++ app/src/main/java/com/example/recyclerview/data.kt
@@ -0,0 +1,21 @@
+package com.example.recyclerview
+
+import java.util.ArrayList
+
+class data (val thudo: String)
+var strName:String= ""
+var strEmail:String = ""
+var strPhone:String = ""
+var list = listOf(data("Hà Nội"),
+    data("Vienna"),
+    data("Brussels"),
+    data("Mexico City"),
+    data("oslo"),
+    data("Yerevan"),
+    data("Baku"),
+    data("New Delhi"),
+    data("Jakarta"),
+    data("Tehran"),
+    data("Beirut"),
+    data("Ma Cao")
+)
